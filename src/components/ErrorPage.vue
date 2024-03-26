bf2c266ccc440e9ee59e02c479bf85f53f918979
tomasdarienko <darienkotomas@gmail.com>
2024-03-26 17:26:52 +0100

    aggiunta della pagina di errore

---
+++ src/components/ErrorPage.vue
@@ -0,0 +1,24 @@
+   
+   <script>
+   export default {
+        name: 'ErrorPage'
+   }
+   </script>
+        <template>
+             <div class="container-lg" id="error-page">
+        <div class="row h-100 justify-content-center align-items-center">
+            <div class="col-12 col-md-8 col-lg-6">
+                <div class="col-12 mb-4">
+                    <img class="w-100" src="/public/img/error-logo.png" alt="Logo">
+                </div>
+                <div class="col-12 text-center text-white mb-4">
+                    <h1 class="super-ocean">Pagina non trovata</h1>
+                    <p>Siamo spiacenti! La pagina che cerchi non è stata trovata in questo server. È stata probabilmente cancellata oppure ai inserito un indirizzo sbagliato. Ti preghiamo di controllare l'URL e riprovare</p>
+                </div>
+            </div>
+        </div>
+    </div>
+           </template>
+   
+   <style scoped>
+   </style>
