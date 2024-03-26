322b3fbea1fbcdc79d4710f8103e02f961da3398
xJustFox <dodydaversa@gmail.com>
2024-03-26 19:01:48 +0100

    [Upgrade] Modificata la home

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -3,26 +3,30 @@ export default {
     name: 'Home'
 }
 </script>
+
 <template lang="">
-    <div class="p-3 pb-lg-5 mb-4 rounded-3">
-        <div class="container-fluid px-3 py-5">
-            <div class="row">
-                <div class="col-lg-6">
-                    <h1 class="display-1 fw-bold super-ocean text-white pt-4 text-center text-lg-start">
+    <div class="container-lg pt-2">
+        <div class="row">
+            <div class="col-12 col-md-6 d-flex flex-column justify-content-center ">
+                <div class="col-12">
+                    <h1 class="super-ocean text-white text-center text-lg-start">
                         Mangia con passione, comodamente a casa:
                         la tua felicità in ogni piatto
                     </h1>
-
-                    <p class="col-lg-7 pt-2 text-lightgray text-center text-lg-start">
+                </div>
+                <div class="col-12">
+                    <p class="text-lightgray text-center text-lg-start">
                         Gusti autentici, comodamente a portata di click. Vivere bene non è mai stato così facile!
                     </p>
                 </div>
-                <div class="col-lg-5 d-flex justify-content-center align-items-center">
-                    <img class="w-lg-50 w-100" src="/public/img/group.png" alt="">
-                </div>
+
+            </div>
+            <div class="col-12 col-md-6">
+                <img class="img-fluid" src="/public/img/group.png" alt="">
             </div>
         </div>
     </div>
 </template>
+
 <style lang="scss" scoped>
 </style>
