353f9230ef03b4aded793b05ef0d8fde3c155ca2
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-03-27 17:52:58 +0100

    [UPDATE] Aggiornate le card

--- src/pages/AppMenu.vue
+++ src/pages/AppMenu.vue
@@ -36,22 +36,30 @@ export default {
 </script>
 
 <template lang="">
-    <div class="container-lg">
+    <div class="container-lg mb-2">
         <div class="row">
             <div class="col-12">
                 <h2 class="super-ocean">Menù</h2>
             </div>
             <div class="col-12 col-md-4 col-lg-3 my-2" v-for="dish in dishes" :key="dish.id">
-                <div class="card text-white h-100">
-                    <div class="card-body my-2">
-                        <div class="text-center">
-                            <img :src="getImage()" alt="">
+                <div class="card h-100">
+                    <div class="card__img">
+                        <div v-if="dish.image != null">
+                            <img :src="dish.image" alt="">
                         </div>
-                        <div class="mx-4 mt-4">
-                            <h5>{{dish.name}}</h5>
-                            <p>{{dish.price}}$</p>
+                    </div>
+                    <div class="card__title">{{dish.name}}</div>
+                    <div class="card__subtitle">{{dish.description}}</div>
+                    <div class="card__wrapper">
+                        <div class="card__price text-center">{{dish.price}}$</div>
+                        <div class="d-flex justify-content-between align-items-center mt-3">
+                            <div class="card__counter">
+                                <button class="card__btn">-</button>
+                                <div class="card__counter-score">0</div>
+                                <button class="card__btn card__btn-plus">+</button>
+                            </div>
+                            <button class="add_btn btn">Aggiungi</button>
                         </div>
-                        <button class="btn btn-sm">Aggiungi</button>
                     </div>
                 </div>
             </div>
@@ -63,21 +71,86 @@ export default {
 @use '../styles/partials/variables' as *;
 
 .card {
-    backdrop-filter: blur(25px);
-    background-color: rgba(255, 255, 255, 0.05);
+    --main-color: #fff;
+    --bg-color: rgba(255, 255, 255, 0.05);
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    padding: 20px;
+    background: var(--bg-color);
+    border-radius: 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
 
-    .card-body {
+    .card__img {
+        width: 170px;
+        height: 170px;
+        background: linear-gradient(90deg, #fff8f79a, #f0f7dfa2);
+        border-radius: 100%;
+        margin-inline: auto;
+        position: relative;
+
         img {
-            width: 200px;
-        }
-
-        button {
-            background-color: $my_orange;
-            color: white;
+            height: 150px;
             position: absolute;
-            bottom: 0;
-            right: 0;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
         }
+    }
+
+    .card__title {
+        font-weight: 600;
+        font-size: 25px;
+        color: var(--main-color);
+    }
+
+    .card__subtitle {
+        font-weight: 400;
+        font-size: 15px;
+        color: var(--main-color);
+        letter-spacing: 0.5px;
+    }
+
+    .card__price {
+        font-weight: 600;
+        font-size: 22px;
+        color: var(--main-color);
+    }
+
+
+    .card__counter {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 15px;
+        padding: 5px;
+        background: #666;
+        border-radius: 50px;
+    }
+
+    .card__counter-score,
+    .card__btn {
+        font-weight: 600;
+        font-size: 22px;
+        color: var(--main-color);
+    }
+
+    .card__btn {
+        width: 30px;
+        height: 30px;
+        border-radius: 100%;
+        border: none;
+        background: transparent;
+    }
+
+    .card__btn-plus {
+        background: var(--bg-color);
+    }
+
+    .add_btn {
+        background-color: $my_orange;
+        color: #fff;
+        border-radius: 50px;
     }
 }
 </style>
