f0e0099c20b4d164208a2500c92ac27a15fdc206
aletardio <ale.tardio99@gmail.com>
2024-04-03 10:04:18 +0200

    [Update] modifiche nel main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router.js'
-import VueMask from 'vue-the-mask'
 
-createApp(App).use(router).use(VueMask).mount('#app')
+createApp(App).use(router).mount('#app')
 
